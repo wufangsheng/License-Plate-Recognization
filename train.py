4b530163f398c6cf8aea228b9c7232f8bd3a6549
wufangsheng <1059609221@qq.com>
2019-06-06 14:35:27 +0800

    Annotated Version

--- train.py
+++ train.py
@@ -7,12 +7,13 @@ from keras.utils import to_categorical
 from keras.layers.core import Dense, Dropout, Activation
 from sklearn.model_selection import train_test_split
 
+#parameters
 batch_size = 400
 epochs = 20
 save_dir = './checkpoints'
 model_name = 'trained_model.h5'
 
-
+#count data size
 def __getnum__(path):
     fm=os.listdir(path)
     i=0
@@ -20,6 +21,7 @@ def __getnum__(path):
         i+=1
     return i  
 
+#data pre-processing
 def __data_label__(path,count): 
     data = np.empty((count,185*594),dtype="float32")
     label = np.empty((count,5),dtype="uint8")
@@ -37,31 +39,36 @@ def __data_label__(path,count):
     label = label.reshape(count,50)
     return data,label
 
+#establish the model
 model = models.Sequential()
 
+#first layer, dense, 64 nodes
 model.add(Dense(64,input_shape=(185*594,)))
-model.add(Activation('relu'))
+model.add(Activation('relu'))#ReLU activation function
 model.add(Dropout(0.5)) #50% dropout
-
+#second layer, dense, 64 nodes
 model.add(Dense(64))
-model.add(Activation('relu'))
+model.add(Activation('relu'))#ReLU activation function
 model.add(Dropout(0.5)) #50% dropout
-
+#third layer, dense, 64 nodes
 model.add(Dense(64))
-model.add(Activation('sigmoid'))
+model.add(Activation('sigmoid'))#Sigmoid activation function to uniform output
 model.add(Dropout(0.5)) #50% dropout
-
+#output layer, dense, 50 outputs
 model.add(Dense(50))
-model.add(Activation('softmax'))
+model.add(Activation('softmax'))#Uniform the output into probabilities(0-1)
 
 path = './data'
 count=__getnum__(path)
 images,labels= __data_label__(path, count)
 
+#Split data into train and test sets at the proportion of 7:3
 X_train, X_test, y_train, y_test = train_test_split(images, labels, test_size=0.3, random_state=30)
 
+#compile the model with Mean Squre Error loss function, RMSprop optimizer
 model.compile(loss='MSE', optimizer='rmsprop', metrics=['accuracy'])
 
+#save checkpoints
 filepath="model_{epoch:02d}-{val_acc:.2f}.hdf5"
 checkpoint = ModelCheckpoint(os.path.join(save_dir, filepath), monitor='val_acc',verbose=1,save_best_only=True)
 
